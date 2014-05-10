3394936fb0bea854e887b664f732ce90af4947c7
Katrina Niolet <katrina.niolet@kdab.com>
2014-05-10 03:44:50 -0500

    Load text information

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,3 +1,5 @@
+#include <QFile>
+#include <QFileDialog>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -6,9 +8,29 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionLoad_Text,SIGNAL(triggered()),this,SLOT(loadText()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::loadText()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Select text"), QString(), tr("Text files (*.txt)"));
+    QFile file(fileName);
+    if(!file.open(QIODevice::ReadOnly)) {
+        statusBar()->showMessage(tr("Could not open file %1").arg(fileName));
+        return;
+    }
+    while(!file.atEnd()) {
+        QByteArray line = file.readLine();
+        if(line.startsWith("Title: "))
+            ui->textTitle->setText(QString::fromUtf8(line.replace("Title: ","").simplified()));
+        else if(line.startsWith("Language: "))
+            ui->textLanguage->setText(QString::fromUtf8(line.replace("Language: ","").simplified()));
+        else if(line.startsWith("***START OF THE PROJECT GUTENBERG"))
+            break;
+    }
 }
